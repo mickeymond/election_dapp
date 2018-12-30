9b06a16911dd1d0c18e70c562624c466eefcc454
mickeymond <mickeymond@gmail.com>
2018-12-30 07:54:34 +0000

    Updates

---
+++ precache-manifest.84bde2f216b55d95c8d8136fb5b25558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb4738c9e2e896fd3e5b",
+    "url": "/election_dapp/static/js/runtime~main.bb4738c9.js"
+  },
+  {
+    "revision": "9ea2c5a4249509fb7909",
+    "url": "/election_dapp/static/js/main.9ea2c5a4.chunk.js"
+  },
+  {
+    "revision": "c5ca83d9fe11cce338d6",
+    "url": "/election_dapp/static/js/1.c5ca83d9.chunk.js"
+  },
+  {
+    "revision": "c5ca83d9fe11cce338d6",
+    "url": "/election_dapp/static/css/1.9a855326.chunk.css"
+  },
+  {
+    "revision": "5d0d23ec53ec88a182a8b81de365b50d",
+    "url": "/election_dapp/index.html"
+  }
+];
