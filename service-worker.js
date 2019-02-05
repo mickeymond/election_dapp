5369a3536dd1868f1201e22d98440555c127f713
mickeymond <mickeymond@gmail.com>
2019-02-05 21:11:52 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/election_dapp/precache-manifest.a350d8714c849af082988ccd9d299945.js"
+  "/election_dapp/precache-manifest.fc3f69580747a6980e3f96986239db7d.js"
 );
 
 workbox.clientsClaim();
